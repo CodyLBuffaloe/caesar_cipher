1c6d69024e33c9b51eaf6a6b10c4c825ea414ea2
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-04-20 13:57:20 -0400

    Barring unforseen edgecases, it is done

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,24 +1,30 @@
 def caesar_cipher(string, shift)
 
     cipher = string.split("")
+    ciphered_text = []
 
     cipher.each do |letter|
 
 		translate = letter.ord + shift
 
-      if(letter == letter.upcase)
+		if(translate > 122)
+		    translate = (letter.ord - 25) + shift
 
-      	return translate.chr.upcase
+		elsif(letter.ord.between?(65,90) && translate > 90)
+		    translate = (letter.ord - 25) + shift
 
-      elsif(translate > 122)
+        elsif(letter.ord.between?(32,64) || letter.ord.between?(91,96))
+		    translate = letter.ord
+		end
 
-      	cycled = letter.ord -25
-      	cycled += shift
-      	return cycled.chr
-      end
-          
+        ciphered_text.push(translate.chr)
+
+
     end
 
-
+    puts ciphered_text.join("")
 
 end
+
+
+caesar_cipher("Zbz",3)
