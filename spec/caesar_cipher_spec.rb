42749a8f846cd340e84ef30013289c3cc8ad97fd
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-06-26 13:48:56 -0700

    Wrote second test, test failed, refactoring code to pass test

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -2,9 +2,16 @@ require "caesar_cipher"
 
 describe CaesarCipher do
   describe ".caesar_cipher" do
-    context "given A, 1" do
-      it "returns B" do
-        expect(CaesarCipher.caesar_cipher("A", 1)).to eql("B")
+    context "given a single letter and 1" do
+      context "given A, 1" do
+        it "returns B" do
+          expect(CaesarCipher.caesar_cipher("A", 1)).to eql("B")
+        end
+      end
+      context "given z, 1" do
+        it "returns a" do
+          expect(CaesarCipher.caesar_cipher("z", 1)).to eql("a")
+        end
       end
     end
   end
